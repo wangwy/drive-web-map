329c5b04958a32b0cf19a1e4a34b619bb59f56eb
wangwy <wangwenyuan@goodow.com>
2014-07-16 11:06:43 +0800

    initial gh-pages

--- app/js/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/* Directives */
-
-angular.module('drive', ['drive.directives','drive.services'])
-    .controller('driveController',['$scope', function($scope){
-      $scope.yourName = '11111';
-    }]);
